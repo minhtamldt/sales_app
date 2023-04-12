1312649c4f4be01206cea9f11aa73cbe52ecf2fa
tamtm <minhtamldt@gmail.com>
2023-04-12 22:32:53 +0700

    add splash screen.

--- lib/presentations/splash/splash_page.dart
+++ lib/presentations/splash/splash_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:lottie/lottie.dart';
+import 'package:sales_app/common/constants/preference_key.dart';
+import 'package:sales_app/common/constants/route_constant.dart';
+
+import '../../data/datasources/local/cache/app_sharepreference.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -10,13 +15,38 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+
+  @override void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    Future.delayed(const Duration(seconds: 2),() {
+      String token = AppSharePreference.getString(PreferenceKey.TOKEN);
+      if (token.isNotEmpty) {
+        Navigator.pushReplacementNamed(context, RouteConstant.HOME);
+      } else {
+        Navigator.pushReplacementNamed(context, RouteConstant.LOGIN);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      color: Colors.red,
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-    );
+    return Container(
+        color: Colors.blueGrey,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Lottie.asset('assets/animations/animation_splash.json',
+                animate: true, repeat: true),
+            const Text("Welcome",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50,
+                    color: Colors.white))
+          ],
+        ));
   }
 
 }
